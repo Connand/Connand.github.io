3c1b198977c41826130d70f98a3ae2aca7bbc1e2
Commande <b10807105@mail.ntust.edu.tw>
2021-09-08 12:42:42 +0800

    Site updated: 2021-09-08 12:42:37

--- schedule.js
+++ schedule.js
@@ -4,41 +4,51 @@ let day = date.getDay(); // getDay returns 0 to 6; 0 = sun, 1 = mon and so on.
 let hour= date.getHours(); // gethhours returns the hour (0-23)
 let result = "";
 
-if(day == 0) {
-    result += "暴食王12人<br>";
-    result += "古龍監獄城<br>";
-    result += "深海王阿斯莫德<br>";
-    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍最深處<br>";
-    //result += '<a class="button--animated" href="" rel="" target="_blank">FB粉絲專頁</a>';
-}
-else if(day == 1) {
-    result += "暴食王12人<br>";
-    result += "古龍最深處<br>";
-}
-else if(day == 2) {
-    result += "蒼蠅王12人<br>";
-    result += "古龍監獄城<br>";
-}
-else if(day == 3) {
-    result += "暴食王12人<br>";
-    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍監獄城<br>";
-}
-else if(day == 4) {
-    result += "蒼鷹王12人<br>";
-    result += "深海王阿斯莫德<br>";
-}
-else if(day == 5) {
-    result += "暴食王12人<br>";
-    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍最深處<br>";
-}
-else if(day == 6) {
-    result += "蒼蠅王12人<br>";
-    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍監獄城<br>";
-    result += "深海王阿斯莫德<br>";
-    result += "古龍最深處<br>";
-}
+let dungeons =
+[
+    {
+        name: "蒼蠅王12人",
+        html: "蒼蠅王12人<br>",
+        days: [2, 6]
+    },
+    {
+        name: "暴食王12人",
+        html: "暴食王12人<br>",
+        days: [0, 1, 3, 5]
+    },
+    {
+        name: "古龍最深處",
+        html: "古龍最深處<br>",
+        days: [1, 6]
+    },
+    {
+        name: "古龍監獄城",
+        html: "古龍監獄城<br>",
+        days: [0, 2]
+    },
+    {
+        name: "[兇夢]古龍最深處",
+        html: "[<font color=#ff0000><b>兇夢</b></font>]古龍最深處<br>",
+        days: [0, 5]
+    },
+    {
+        name: "[兇夢]古龍監獄城",
+        html: "[<font color=#ff0000><b>兇夢</b></font>]古龍監獄城<br>",
+        days: [3, 6]
+    },
+    {
+        name: "深海王阿斯莫德",
+        html: "深海王阿斯莫德<br>",
+        days: [0, 4, 6]
+    },
+    
+];
 
-
+dungeons.forEach((e, i) => {
+    if(e.days.includes(day)){
+        result += e.html;
+    }
+});
 
 //outut the result to div
   document.getElementById("schedule").innerHTML = result;
