b22cd358625fda80184780ad747145b697d4ee4a
Commande <b10807105@mail.ntust.edu.tw>
2021-09-08 11:10:48 +0800

    Site updated: 2021-09-08 11:10:38

--- schedule.js
+++ schedule.js
@@ -8,7 +8,7 @@ if(day == 0) {
     result += "暴食王12人<br>";
     result += "古龍監獄城<br>";
     result += "深海王阿斯莫德<br>";
-    result += "[兇夢]古龍最深處<br>";
+    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍最深處<br>";
     //result += '<a class="button--animated" href="" rel="" target="_blank">FB粉絲專頁</a>';
 }
 else if(day == 1) {
@@ -21,7 +21,7 @@ else if(day == 2) {
 }
 else if(day == 3) {
     result += "暴食王12人<br>";
-    result += "[兇夢]古龍監獄城<br>";
+    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍監獄城<br>";
 }
 else if(day == 4) {
     result += "蒼鷹王12人<br>";
@@ -29,11 +29,11 @@ else if(day == 4) {
 }
 else if(day == 5) {
     result += "暴食王12人<br>";
-    result += "[兇夢]古龍最深處<br>";
+    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍最深處<br>";
 }
 else if(day == 6) {
     result += "蒼蠅王12人<br>";
-    result += "[兇夢]古龍監獄城<br>";
+    result += "[<font color=#ff0000><b>兇夢</b></font>]古龍監獄城<br>";
     result += "深海王阿斯莫德<br>";
     result += "古龍最深處<br>";
 }
